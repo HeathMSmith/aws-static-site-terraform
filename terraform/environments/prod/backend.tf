4c529baa4bb66f29b40229eb653c1565d699f0e2
Heath Smith <heathsmithaws2025@gmail.com>
2026-08-10 09:37:53 -0400

    chore: migrate Terraform state locking to S3

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "hms-terraform-state-portfolio"
-    key            = "static-site/prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "hms-terraform-locks"
-    encrypt        = true
+    bucket       = "hms-terraform-state-portfolio"
+    key          = "static-site/prod/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
